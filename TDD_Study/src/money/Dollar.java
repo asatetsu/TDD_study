00a2a743cffe7542848a93131f08c4fcc111be1e
tetsuo asada <tetsuoasada@tetsuonoMacBook-Pro.local>
2017-12-24 23:44:17 +0900

    Part1最終版

--- TDD_Study/src/money/Dollar.java
+++ TDD_Study/src/money/Dollar.java
@@ -3,10 +3,10 @@ package money;
 class Dollar {
 	int amount;	
 	Dollar(int amount){
-		
+		this.amount = amount;
 	}
 	void times(int multiplier) {
-		amount = 5*2;
+		amount *= multiplier;
 	}
 
 	
