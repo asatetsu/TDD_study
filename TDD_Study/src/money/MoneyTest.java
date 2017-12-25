4f24cb3d15b74fc6ea521971007dc8ce981583b6
tetsuo asada <tetsuoasada@tetsuonoMacBook-Pro.local>
2017-12-25 23:50:43 +0900

    Francのテストとコードを追加

--- TDD_Study/src/money/MoneyTest.java
+++ TDD_Study/src/money/MoneyTest.java
@@ -6,10 +6,8 @@ import static org.junit.jupiter.api.Assertions.*;
 public class MoneyTest {
 	@Test
 	public void testMultiplication() {
-		Dollar five = new Dollar(5);
-		//Dollar product = five.times(2);
-		assertEquals(new Dollar(10),five.times(2));
-		//product = five.times(3);
+		Dollar five = new Dollar(5);		//Dollar product = five.times(2);
+		assertEquals(new Dollar(10),five.times(2));		//product = five.times(3);
 		assertEquals(new Dollar(15),five.times(3));
 	}
 	
@@ -17,6 +15,13 @@ public class MoneyTest {
 	public void testQquality() {
 		assertTrue(new Dollar(5).equals(new Dollar(5)));
 		assertFalse(new Dollar(5).equals(new Dollar(6)));
+	}
+	
+	@Test
+	public void testFrancMultiplication() {
+		Franc five = new Franc(5);
+		assertEquals(new Franc(10),five.times(2));	
+		assertEquals(new Franc(15),five.times(3));		
 	}
 }
 
