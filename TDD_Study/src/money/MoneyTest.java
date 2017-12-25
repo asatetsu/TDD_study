c9349a4217db6975741dd7e2c7fa4cdaa9294483
tetsuo asada <uniqueisi@gmail.com>
2017-12-26 06:48:28 +0900

    P47-49 dollar メソッド追加 Moneyクラスをabstractクラスに変更

--- TDD_Study/src/money/MoneyTest.java
+++ TDD_Study/src/money/MoneyTest.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public class MoneyTest {
 	@Test
 	public void testMultiplication() {
-		Dollar five = new Dollar(5);		//Dollar product = five.times(2);
+		Money five = Money.dollar(5);		//Dollar product = five.times(2);
 		assertEquals(new Dollar(10),five.times(2));		//product = five.times(3);
 		assertEquals(new Dollar(15),five.times(3));
 	}
