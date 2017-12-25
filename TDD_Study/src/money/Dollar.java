9a6e7fbe45676cbd1f7e30d442141f21a8124faa
tetsuo asada <uniqueisi@gmail.com>
2017-12-26 06:26:30 +0900

    Dollar クラスのequalsメソッドをMoneyクラスに引き上げ

--- TDD_Study/src/money/Dollar.java
+++ TDD_Study/src/money/Dollar.java
@@ -9,10 +9,5 @@ class Dollar extends Money{
 	Dollar times(int multiplier) {
 		return new Dollar(amount * multiplier);
 	}
-
-	public boolean equals(Object object) {
-		Dollar dollar = (Dollar) object;
-		return amount == dollar.amount;
-	}
 	
 }
