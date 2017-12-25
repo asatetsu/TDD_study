393caaee5b55baa2b6df17d5c848165b2e885112
tetsuo asada <uniqueisi@gmail.com>
2017-12-26 06:31:02 +0900

    Francクラスの等価性テスト追加

--- TDD_Study/src/money/MoneyTest.java
+++ TDD_Study/src/money/MoneyTest.java
@@ -15,6 +15,9 @@ public class MoneyTest {
 	public void testQquality() {
 		assertTrue(new Dollar(5).equals(new Dollar(5)));
 		assertFalse(new Dollar(5).equals(new Dollar(6)));
+		
+		assertTrue(new Franc(5).equals(new Franc(5)));
+		assertFalse(new Franc(5).equals(new Franc(6)));
 	}
 	
 	@Test
