a7210f038ebfbee16dfd4a8518792305407b665b
tetsuo asada <uniqueisi@gmail.com>
2018-01-06 00:34:32 +0900

    P144-145 testMethod呼出実装 WasRunクラス作成

--- Part2/xunit.py
+++ Part2/xunit.py
@@ -4,3 +4,14 @@
 # TODO: []テストメソッドが失敗したとしてもtearDownを呼び出す
 # TODO: []複数のテストを走らせる
 # TODO: []収集したテスト結果を出力する
+class WasRun:
+	def __init__(self, name):
+		self.wasRun = None
+	def testMethod(self):
+		self.wasRun = 1
+
+
+test = WasRun("testMethod")
+print(test.wasRun)
+test.testMethod()
+print(test.wasRun)
