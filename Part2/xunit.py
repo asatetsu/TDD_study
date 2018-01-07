ff932d183ed4935050bfbfd8bf63fdb46beb6644
tetsuo asada <uniqueisi@gmail.com>
2018-01-08 08:38:56 +0900

    テストメソッド呼び出し完了時のコードコミット

--- Part2/xunit.py
+++ Part2/xunit.py
@@ -1,4 +1,4 @@
-# TODO: []テストメソッドを呼び出す
+# TODO: [x]テストメソッドを呼び出す
 # TODO: []setUpを最初に呼び出す
 # TODO: []tearDownをあとで呼び出す
 # TODO: []テストメソッドが失敗したとしてもtearDownを呼び出す
@@ -18,8 +18,11 @@ class WasRun(TestCase):
 	def testMethod(self):
 		self.wasRun = 1
 
+class TestCaseTest(TestCase):
+	def testRunning(self):
+		test = WasRun("testMethod")
+		assert(not test.wasRun)
+		test.run()
+		assert(test.wasRun)
 
-test = WasRun("testMethod")
-print(test.wasRun)
-test.run()
-print(test.wasRun)
+TestCaseTest("testRunning").run()
