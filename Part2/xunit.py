d05f5e2470571247a689ce76dffbee5d77b31887
tetsuo asada <uniqueisi@gmail.com>
2018-01-08 13:50:54 +0900

    テスト実行数をテスト実行毎にインクリメントするように変更

--- Part2/xunit.py
+++ Part2/xunit.py
@@ -13,11 +13,13 @@ class TestCase:
 	def tearDown(self):
 		pass
 	def run(self):
+		result = TestResult()
+		result.testStarted()
 		self.setUp()
 		method = getattr(self,self.name)
 		method()
 		self.tearDown()
-		return TestResult()
+		return result
 
 
 class WasRun(TestCase):
@@ -30,7 +32,9 @@ class WasRun(TestCase):
 
 class TestResult:
 	def __init__(self):
-		self.runCount = 1
+		self.runCount = 0
+	def testStarted(self):
+		self.runCount = self.runCount + 1
 	def summary(self):
 		return "%d run, 0 failed" % self.runCount
 
